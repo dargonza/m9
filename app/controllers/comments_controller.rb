fa55c872aaac04c84f17fa39176c7a90a547570e
Darío González <dario.gonzalez@outlook.com>
2021-11-24 23:01:54 -0300

    Login

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,21 @@
 class CommentsController < ApplicationController
     def create
         @article = Article.find(params[:article_id])        
-        @comment =  @article.comments.create(comment_param)    
+        @comment =  @article.comments.create(comment_params)    
         
         redirect_to articles_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:article_id])
-        @comment = @article.comments.find(params[:id])}
+        @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
     end
 
-    private    
-        def comment_param
-            param.require(:comment).permit(:commenter, :body)
-        end
+    
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end
+        
 end
